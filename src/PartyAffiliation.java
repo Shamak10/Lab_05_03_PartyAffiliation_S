af4f7066157516524927b37585a4cda197626d40
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-02-11 11:15:55 -0500

    Update PartyAffiliation.java

--- src/PartyAffiliation.java
+++ src/PartyAffiliation.java
@@ -1,16 +1,12 @@
-import java.util.Scanner;
 public class PartyAffiliation {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter your party affiliation (D, R, or I): ");
-        //Output is choose the party Symbol
-        String party =("S");
+        String party = "S";//Choose the symbol you want
 
-        if (party ==("D")) {// Input is if you select D
+        if (party.equals("D")) {// Input is if you select D
             System.out.println("You get a Democratic Donkey.");// output you get Democratic Donkey
-        } else if (party ==("R")) {//Input is if you select R
+        } else if (party.equals("R")) {// Input is if you select R
             System.out.println("You get a Republican Elephant.");// output you get Republican Elephant
-        } else if (party ==("I")) {//Input is if you select I
+        } else if (party.equals("I")) {// Input is if you select I
             System.out.println("You get an Independent Person.");// output you get Independent Person
         } else {// Input is if you select any other letter
             System.out.println("You get an Other.");//Output you get other
